e397a21ffc8bf392ccc3c5c1801cd30c8830fff0
Emanuele-Saccoliti <saccolitiemanuele@gmail.com>
2026-09-24 14:39:40 +0200

    M04: implement SSVI total variance kernel

--- vv_pricer/surface/__init__.py
+++ vv_pricer/surface/__init__.py
@@ -1,12 +1,20 @@
 """Inputs produced for later volatility-surface construction."""
 
+from .ssvi import (
+    SsviPowerLawParameters,
+    ssvi_power_law_phi,
+    ssvi_total_variance,
+)
 from .types import SmilePointProvenance, VvSmileSample, VvSmileSamplePoint
 from .vv_sampler import VannaVolgaSmileSampler, VvSmileSamplingConfig
 
 __all__ = [
     "SmilePointProvenance",
+    "SsviPowerLawParameters",
     "VannaVolgaSmileSampler",
     "VvSmileSample",
     "VvSmileSamplePoint",
     "VvSmileSamplingConfig",
+    "ssvi_power_law_phi",
+    "ssvi_total_variance",
 ]
